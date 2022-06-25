608b23aee3a704c6c432335842804963c020fdc5
BaxtiyorErkinov <baxa10th@gmail.com>
2022-06-25 21:05:08 +0500

    switch case

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 
 
 func main() {
-    mess, allowed := enterTheClub(6)
-    fmt.Println(mess, allowed)
+    // mess, allowed := enterTheClub(6)
+    // fmt.Println(mess, allowed)
+    fmt.Println(prediction(10))
 
 }
 
@@ -21,4 +22,25 @@ func enterTheClub(age int) (string, error) {
         return "You are very old", errors.New("You are very old!!!")
     }
     return "Closed", errors.New("You are very young")
+}
+
+func prediction(dayOfWeek int) (string, error) {
+    switch dayOfWeek {
+        case 1: 
+            return "Monday", nil
+        case 2:
+            return "Tuesday", nil
+        case 3: 
+            return "Wednesday", nil
+        case 4: 
+            return "Thursday", nil
+        case 5:
+            return "Friday", nil
+        case 6: 
+            return "Saturday", nil
+        case 7:
+            return "Sunday", nil
+        default:
+            return "Hapshanba", errors.New("Invalid day of the week")
+    }
 }
