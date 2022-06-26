24965ed58df138f3106820575179f0f8fbf11904
BaxtiyorErkinov <baxa10th@gmail.com>
2022-06-26 11:13:13 +0500

    pointer

--- main.go
+++ main.go
@@ -4,27 +4,32 @@ import (
     "fmt"
 )
 
-var message string
-
-
 func main() {
-    fmt.Println(message)
 
-    res := increment()
-    fmt.Println(res())
-    fmt.Println(res())
-    fmt.Println(res())
-    fmt.Println(res())
+    text := 77
+    fmt.Println(&text) // 0xc000098000
+
+    address := &text // 0xc000098000
+    fmt.Println(*address) // 77
+    fmt.Println(*&text) // 77
+    fmt.Printf("address это %T\n", &text) // *int
+    var el *int //   здесь он ожидает pointer (ссылку). Например: &text
+    fmt.Println(el) // nil
+
+
+
+    mess := "Hello World" // 0x1234
+    fmt.Println(mess) // 0x1234
+    changeMess(&mess)
+    fmt.Println(mess) // 0x1234
 }
 
-func increment() func() int {
-    count := 0
-    return func() int {
-        count++
-        return count
-    } 
-} 
 
-func init() {
-    message = "hello world"
+
+func changeMess(mess *string) { // 0x1234
+    *mess += "Say Hello"
+}
+
+func printMess(mess string) {
+    fmt.Println(mess) // 0x12345
 }
